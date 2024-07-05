90bb76c1e8e8121375bdde08ea810d7ddf0fad61
Nylesssss <2337620979@qq.com>
2024-07-05 20:41:46 +0800

    1

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,80 +1,81 @@
 export const footerSocials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
-    icon: "ic:baseline-discord",
-  },
-]
-
-export const footerLists = [
 	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
+	  name: 'Twitter',
+	  url: 'https://twitter.com/littlesticksdev',
+	  icon: "mdi:wechat",
 	},
 	{
-		title: 'Company',
-		items: [
-			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
-				title: 'Contact',
-				slug: '/company/contact',
-			},
-		],
+	  name: 'Youtube',
+	  url: 'https://youtube.com/c/LittleSticks',
+	  icon: "mdi:youtube",
 	},
 	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
+	  name: 'Github',
+	  url: 'https://github.com/littlesticks',
+	  icon: "mdi:github",
 	},
-];
+	{
+	  name: 'Discord',
+	  url: 'https://littlesticks.dev/discord',
+	  icon: "ic:baseline-discord",
+	},
+  ]
+  
+  export const footerLists = [
+	  {
+		  title: '分页',
+		  items: [
+			  {
+				  title: '分页 1',
+				  slug: '/landing-pages/landing-1',
+			  },
+			  {
+				  title: '分页 2',
+				  slug: '/landing-pages/landing-2',
+			  },
+		// {
+			  // 	title: 'Landing Page 3',
+			  // 	slug: '/landing-pages/landing-3',
+			  // },
+		  ],
+	  },
+	  {
+		  title: '公司',
+		  items: [
+			  {
+				  title: '关于',
+				  slug: '/company/about',
+			  },
+			  {
+				  title: '博客',
+				  slug: '/blog',
+			  },
+			  {
+				  title: '联系',
+				  slug: '/company/contact',
+			  },
+		  ],
+	  },
+	  {
+		  title: '主题',
+		  items: [
+			  {
+				  title: '开始尝试',
+				  slug: '/theme/get-started',
+			  },
+			  {
+				  title: '风格教学',
+				  slug: '/theme/style-guide',
+			  },
+		{
+		  title: '主题设置',
+		  slug: '/theme/theme-setup',
+		},
+		{
+		  title: '自定义',
+		  slug: '/theme/customizing-odyssey',
+		}
+		  ],
+	  },
+  ];
+  
